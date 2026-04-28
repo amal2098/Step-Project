796aa29e48ff95e319f07331b457c4771522518f
amal2098 <amalalbarqe.a.e@gmail.com>
2026-04-28 23:03:17 +0300

    Update database.py

--- database.py
+++ database.py
@@ -2,10 +2,16 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+# التعديل هنا: غيرنا DATABASE_URL إلى MYSQL_URL لتطابق Railway
 SQLALCHEMY_DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "mysql+pymysql://root:@localhost:3306/step_app_db",
+    "MYSQL_URL", 
+    "mysql+pymysql://root:@localhost:3306/step_app_db"
 )
+
+# ملاحظة: إذا كان الرابط من ريلواي يبدأ بـ mysql:// 
+# SQLAlchemy أحياناً تحتاج تعديله يدوياً ليصبح mysql+pymysql://
+if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("mysql://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("mysql://", "mysql+pymysql://", 1)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
@@ -14,7 +20,6 @@ engine = create_engine(
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
